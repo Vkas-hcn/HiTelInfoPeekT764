38ec1134f8fe507511b0d6ae0afbd5cc8d93b359
vkas <vkas@qq.com>
2025-12-09 18:29:15 +0800

    feat: baibao-1

--- app/src/main/java/com/endless/boundaries/egelixi/AllAppInfor.kt
+++ app/src/main/java/com/endless/boundaries/egelixi/AllAppInfor.kt
@@ -1,12 +1,6 @@
 package com.endless.boundaries.egelixi
 
 import android.app.Application
-import android.content.ComponentName
-import android.content.Context
-import android.content.pm.PackageManager
-import android.util.Log
-import com.clean.dependency.Core
-import com.tencent.mmkv.MMKV
 
 class AllAppInfor: Application() {
     override fun onCreate() {
